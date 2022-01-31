c1c8a80d773ec076f87ec868154164750b80a881
anashandaru <anashandaru256@gmail.com>
2022-01-31 16:55:58 +0800

    multiple cut mseed

--- cut.py
+++ cut.py
@@ -1,21 +1,32 @@
-from curses import echo
-from email.mime import base
 from obspy import read
 from obspy.core import UTCDateTime
 from os.path import basename
 import click
 
+def readStreamList(filename):
+    result = []
+    for item in filename:
+        result.append(read(item))
+    return result
 
-def getStartEnd(st):
-    return [st[0].stats.starttime, st[0].stats.endtime]
+def getStartEnd(streamList):
+    starttimeList = []
+    endtimeList = []
+    for stream in streamList:
+        for trace in stream:
+            starttimeList.append(trace.stats.starttime)
+            endtimeList.append(trace.stats.endtime)
+    start = max(starttimeList)
+    end = min(endtimeList)
+    return [start, end]
 
 def write2file(stream, filename):
     stream.write(filename, format="MSEED")
 
 @click.command()
-@click.argument('filename', type=click.Path(exists=True))
+@click.argument('filename', type=click.Path(exists=True),nargs=-1)
 def main(filename):
-    stream = read(filename)
+    stream = readStreamList(filename)
     minStart, maxEnd = getStartEnd(stream)
     minStart = str(minStart)[:19]
     maxEnd = str(maxEnd)[:19]
@@ -23,8 +34,9 @@ def main(filename):
     end = click.prompt('Endtime [max/example= {} ]'.format((maxEnd)), type=str)
     start = UTCDateTime(start)
     end = UTCDateTime(end)
-    stream.trim(start, end)
-    write2file(stream, "cut-"+basename(filename))
+    for i, item in enumerate(stream):
+        item.trim(start, end)
+        write2file(item, "cut-"+basename(filename[i]))
     click.echo("File is cutted")
  
 if __name__ == '__main__':
